1b7891aedfb30e20938441db60b5ca8093e4f6c0
Sapphire <sapphireyed@gmail.com>
2020-08-27 21:02:43 +0200

    responsivness improved + setinterval on mage pic

--- rogue.js
+++ rogue.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
     $("p", this).toggle();
   });
 });
+
+
+function change() {
+      var i = 0;
+      let pics = ["img/mage_lesson.png", "img/mage_sword.png", "img/mage_potion.png"];
+      var el = document.getElementById('mage1');
+      let pic= pics[Math.floor(Math.random() * pics.length)]
+      if (i < pics.length) {
+        el.src = pic;
+        i++;
+      } else {
+        i = 0;
+      }
+    }
+    setInterval(change, 2000);
